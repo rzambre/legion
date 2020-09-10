52b1263f0167bf8369625437032ee8c454b48048
Rohit Zambre <rzambre@uci.edu>
2020-09-10 14:33:30 +0000

    comm-based parallelism

--- runtime/realm/mpi/am_mpi.cc
+++ runtime/realm/mpi/am_mpi.cc
@@ -15,6 +15,11 @@
 
 #include "am_mpi.h"
 
+#define COMM_BASED_PAR 1 /* 0: tag-based parallelism */
+
+#if COMM_BASED_PAR
+#define NUM_COMMS 20
+#endif
 
 static MPI_Win g_am_win = MPI_WIN_NULL;
 static void *g_am_base = NULL;
@@ -23,16 +28,28 @@ static __thread int thread_id = 0;
 static __thread int am_seq = 0;
 static Realm::atomic<unsigned int> num_threads(0);
 //static Realm::atomic<unsigned int> recv_num_threads(0);
+#if COMM_BASED_PAR
+static unsigned char buf_recv_list[NUM_COMMS][AM_BUF_COUNT][1024];
+static unsigned char *buf_recv[NUM_COMMS];
+static MPI_Request req_recv_list[NUM_COMMS][AM_BUF_COUNT];
+#else
 static unsigned char buf_recv_list[AM_BUF_COUNT][1024];
 static unsigned char *buf_recv = buf_recv_list[0];
 static MPI_Request req_recv_list[AM_BUF_COUNT];
+#endif
 static int n_am_mult_recv = 5;
 static int pre_initialized;
 static int node_size;
 static int node_this;
+#if COMM_BASED_PAR
+static MPI_Comm parapoint[NUM_COMMS];
+static int i_recv_list[NUM_COMMS] = {0};
+static int comm_start = 0;
+#else
 static MPI_Comm multi_vci_comm;
 static MPI_Comm comm_medium;
 int i_recv_list = 0;
+#endif
 
 namespace Realm {
 namespace MPI {
@@ -62,28 +79,45 @@ void AM_Init(int *p_node_this, int *p_node_size)
     
     /* Request for VCIs */
     MPI_Info_create(&info);
+    MPI_Info_set(info, "mpi_assert_new_vci", "true");
+#if COMM_BASED_PAR
+    MPI_Info_set(info, "mpi_num_vcis", "1");
+    for (int i = 0; i < NUM_COMMS; i++) { /* TODO: do we know the number of threads here? */
+        MPI_Comm_dup_with_info(MPI_COMM_WORLD, info, &parapoint[i]);
+    }
+#else
     MPI_Info_set(info, "mpi_assert_unordered", "true"); /* Can use tags for VCI selection */
     
     MPI_Info_set(info, "mpi_assert_tag_based_parallelism", "true"); // tag-based parallelism possible
     MPI_Info_set(info, "mpi_num_tag_bits_for_vci", "5"); // number of bits in the MPI tag to use for VCI selection
     MPI_Info_set(info, "mpi_num_tag_bits_for_app", "5"); // number of bits in the MPI tag for the app
     
-    MPI_Info_set(info, "mpi_assert_new_vci", "true");
     MPI_Info_set(info, "mpi_num_vcis", "10"); /* TODO: do we know the number of threads here? */
     /* TODO: hints to negotiate bits for VCI hashing and user tag*/
     MPI_Comm_dup_with_info(MPI_COMM_WORLD, info, &multi_vci_comm);
-
+#endif
+    
     s = getenv("AM_MULT_RECV");
     if (s) {
         n_am_mult_recv = atoi(s);
     }
     //printf("Rank %d: n_am_mult_recv is %d\n", node_this, n_am_mult_recv);
+#if COMM_BASED_PAR
+    for (int comm_i = 0; comm_i<NUM_COMMS; comm_i++) {
+        for (int  i = 0; i<n_am_mult_recv; i++) {
+            CHECK_MPI( MPI_Irecv(buf_recv_list[comm_i][i], 1024, MPI_CHAR, MPI_ANY_SOURCE, 0x1, parapoint[comm_i], &req_recv_list[comm_i][i]) );
+        }
+        buf_recv[comm_i] = buf_recv_list[comm_i][0];
+    }
+#else
     for (int  i = 0; i<n_am_mult_recv; i++) {
         CHECK_MPI( MPI_Irecv(buf_recv_list[i], 1024, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, multi_vci_comm, &req_recv_list[i]) );
     }
     MPI_Comm_dup(multi_vci_comm, &comm_medium); /* comm_medium inherits VCIs of multi_vci_comm */
+#endif
 
     MPI_Info_free(&info);
+    printf("Initialized!\n");
 }
 
 void AM_Finalize()
@@ -102,8 +136,14 @@ void AM_Finalize()
     }
 
     AMPoll_cancel();
+#if COMM_BASED_PAR
+    for (int comm_i = 0; comm_i<NUM_COMMS; comm_i++) {
+        CHECK_MPI( MPI_Comm_free(&parapoint[comm_i]) );
+    }
+#else
     CHECK_MPI( MPI_Comm_free(&comm_medium) );
     CHECK_MPI( MPI_Comm_free(&multi_vci_comm) );
+#endif
 
     if (!pre_initialized) {
         MPI_Finalize();
@@ -125,6 +165,73 @@ void AMPoll()
         recv_thread_id = recv_num_threads.fetch_add_acqrel(1) + 1;
     }*/
 
+#if COMM_BASED_PAR
+    while (1) {
+        int got_am;
+        int comm_i;
+        MPI_Comm this_parapoint;
+        MPI_Status status;
+        for (int i = 0; i<NUM_COMMS; i++) {
+            got_am = 0;
+            comm_i = (comm_start + i) % NUM_COMMS;
+            //printf("Rank %d: Polling request %d on comm %d\n", node_this, i_recv_list[comm_i], comm_i);
+            CHECK_MPI( MPI_Test(&req_recv_list[comm_i][i_recv_list[comm_i]], &got_am, &status) ); 
+            if (got_am) {
+                msg = (struct AM_msg *) buf_recv[comm_i];
+                tn_src = status.MPI_SOURCE;
+                this_parapoint = parapoint[comm_i];
+                
+                /* Update comm_start for next poll */
+                comm_start = (comm_i + 1) % NUM_COMMS;
+
+                break;
+            }
+        }
+        if (!got_am) {
+            break;
+        }
+
+        char *header;
+        char *payload;
+        int payload_type = 0;
+        if (msg->type == 0) {
+            header = msg->stuff;
+            payload = msg->stuff + msg->header_size;
+        } else if (msg->type == 1) {
+            header = msg->stuff + 4;
+
+            int msg_tag = *(int32_t *)(msg->stuff);
+            payload = (char *) malloc(msg->payload_size);
+            payload_type = 1;    // need_free;
+            //printf("Rank %d, recv_thread %d: Receiving msg type 1\n", node_this, recv_thread_id);
+            //printf("Rank %d: Receiving payload from %d on comm %d, tag %d\n", node_this, tn_src, comm_i, msg_tag);
+            CHECK_MPI( MPI_Recv(payload, msg->payload_size, MPI_BYTE, tn_src, msg_tag, this_parapoint, &status) );
+            //printf("Rank %d: DONE Receiving payload from %d on comm %d, tag %d\n", node_this, tn_src, comm_i, msg_tag);
+        } else if (msg->type == 2) {
+            int offset = *(int32_t *)(msg->stuff);
+            header = msg->stuff + 4;
+            payload = (char *) g_am_base + offset;
+        } else {
+            assert(0 && "invalid message type");
+            header = 0;
+            payload = 0;
+            msg->msgid = 0x7fff; // skips handler below
+        }
+
+        if (msg->msgid != 0x7fff) {
+            Realm::ActiveMessageHandlerTable::MessageHandler handler = Realm::activemsg_handler_table.lookup_message_handler(msg->msgid);
+            (*handler) (tn_src, header, payload, msg->payload_size);
+        }
+
+        if (payload_type == 1) {
+            free(payload);
+        }
+        //printf("Rank %d, recv_thread %d: Posting replacement ANY_TAG receive\n", node_this, recv_thread_id);
+        CHECK_MPI( MPI_Irecv(buf_recv_list[comm_i][i_recv_list[comm_i]], 1024, MPI_CHAR, MPI_ANY_SOURCE, 0x1, parapoint[comm_i], &req_recv_list[comm_i][i_recv_list[comm_i]]) );
+        i_recv_list[comm_i] = (i_recv_list[comm_i] + 1) % n_am_mult_recv;
+        buf_recv[comm_i] = buf_recv_list[comm_i][i_recv_list[comm_i]];
+    }
+#else
     while (1) {
         int got_am;
         MPI_Status status;
@@ -174,14 +281,23 @@ void AMPoll()
         i_recv_list = (i_recv_list + 1) % n_am_mult_recv;
         buf_recv = buf_recv_list[i_recv_list];
     }
+#endif
 
 }
 
 void AMPoll_cancel()
 {
+#if COMM_BASED_PAR
+    for (int comm_i = 0; comm_i<NUM_COMMS; comm_i++) {
+        for (int  i = 0; i<n_am_mult_recv; i++) {
+            CHECK_MPI( MPI_Cancel(&req_recv_list[comm_i][i]) );
+        }
+    }
+#else
     for (int  i = 0; i<n_am_mult_recv; i++) {
         CHECK_MPI( MPI_Cancel(&req_recv_list[i]) );
     }
+#endif
 }
 
 void AMSend(int tgt, int msgid, int header_size, int payload_size, const char *header, const char *payload, int has_dest, MPI_Aint dest)
@@ -210,8 +326,15 @@ void AMSend(int tgt, int msgid, int header_size, int payload_size, const char *h
         int n = AM_MSG_HEADER_SIZE + 4 + header_size;
         assert(tgt != node_this);
         //printf("Rank %d, thread %d: AMSend sending a message of type 2 after a Put-Win_flush\n", node_this, thread_id);
-        int msg_tag = (thread_id << 10) + 1;
+
+        //printf("Rank %d: Sending to %d with dest using thread %d on comm %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS);
+#if COMM_BASED_PAR
+        CHECK_MPI( MPI_Send(buf_send, n, MPI_BYTE, tgt, 0x1, parapoint[(thread_id-1)%NUM_COMMS]) );
+#else
+        int msg_tag = (thread_id << 10) + 1; /* TODO: use variable for number of bits to shift */
         CHECK_MPI( MPI_Send(buf_send, n, MPI_BYTE, tgt, msg_tag, multi_vci_comm) );
+#endif
+        //printf("Rank %d: DONE Sending to %d with dest using thread %d on comm %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS);
     } else if (AM_MSG_HEADER_SIZE + header_size + payload_size < 1024) {
         msg->type = 0;
         if (header_size > 0) {
@@ -223,8 +346,14 @@ void AMSend(int tgt, int msgid, int header_size, int payload_size, const char *h
         int n = AM_MSG_HEADER_SIZE + header_size + payload_size;
         assert(tgt != node_this);
         //printf("Rank %d, thread %d: AMSend sending a message of type 0\n", node_this, thread_id);
-        int msg_tag = (thread_id << 10) + 1;
+        //printf("Rank %d: Sending to %d msg type 0 using thread %d on comm %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS);
+#if COMM_BASED_PAR
+        CHECK_MPI( MPI_Send(buf_send, n, MPI_CHAR, tgt, 0x1, parapoint[(thread_id-1)%NUM_COMMS]) );
+#else
+        int msg_tag = (thread_id << 10) + 1; /* TODO: use variable for number of bits to shift */
         CHECK_MPI( MPI_Send(buf_send, n, MPI_CHAR, tgt, msg_tag, multi_vci_comm) );
+#endif
+        //printf("Rank %d: DONE Sending to %d msg type 0 using thread %d on comm %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS);
     } else {
         msg->type = 1;
         int msg_tag = 0x0;
@@ -233,16 +362,28 @@ void AMSend(int tgt, int msgid, int header_size, int payload_size, const char *h
         //}
         am_seq = (am_seq + 1) & 0x1f;
         msg_tag = (thread_id << 10) + am_seq;
+        
         *((int32_t *) msg_header) = (int32_t) msg_tag;
         memcpy(msg_header + 4, header, header_size);
         int n = AM_MSG_HEADER_SIZE + 4 + header_size;
         assert(tgt != node_this);
         //printf("Rank %d, thread %d: AMSend sending a message of type 1; first send\n", node_this, thread_id);
+        //printf("Rank %d: Sending to %d msg type 1 header using thread %d on comm %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS);
+#if COMM_BASED_PAR
+        CHECK_MPI( MPI_Send(buf_send, n, MPI_BYTE, tgt, 0x1, parapoint[(thread_id-1)%NUM_COMMS]) );
+#else
         CHECK_MPI( MPI_Send(buf_send, n, MPI_BYTE, tgt, msg_tag, multi_vci_comm) );
+#endif
+        //printf("Rank %d: DONE Sending to %d msg type 1 header using thread %d on comm %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS);
         assert(tgt != node_this);
         //printf("Rank %d, thread %d: AMSend sending a message of type 1; second send on comm_medium\n", node_this, thread_id);
+        //printf("Rank %d: Sending to %d msg type 1 payload using thread %d on comm %d, tag %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS, msg_tag);
+#if COMM_BASED_PAR
+        CHECK_MPI( MPI_Send(payload, payload_size, MPI_BYTE, tgt, msg_tag, parapoint[(thread_id-1)%NUM_COMMS]) );
+#else
         CHECK_MPI( MPI_Send(payload, payload_size, MPI_BYTE, tgt, msg_tag, comm_medium) );
-
+#endif
+        //printf("Rank %d: DONE Sending to %d msg type 1 payload using thread %d on comm %d, tag %d\n", node_this, tgt, thread_id, (thread_id-1)%NUM_COMMS, msg_tag);
     }
 }
 
